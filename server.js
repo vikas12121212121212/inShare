0daa75d6979c4fd0ccfccec3867bb830f7686cf4
vikas <vikasbiradar910@gmail.com>
2024-07-09 18:05:05 +0530

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express=require('express');
+require('dotenv').config();
+
 const app = express();
 const path = require('path');
 const PORT=process.env.PORT || 3000;
-
+const cors = require('cors');
 app.use(express.static('public'));
 app.use(express.json());
+
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions));
 
 const connectDB = require('./config/db');
 connectDB();
