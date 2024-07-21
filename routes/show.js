043932b73ffeadc4e828acebd0e47fcbc2507790
vikas <vikasbiradar910@gmail.com>
2024-07-21 23:58:35 +0530

    hi

--- routes/show.js
+++ routes/show.js
@@ -1,24 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const File = require('../models/file'); // Adjust the path as needed
+const router = require('express').Router();
+const File = require('../models/file');
 
 router.get('/:uuid', async (req, res) => {
     try {
         const file = await File.findOne({ uuid: req.params.uuid });
-        if (!file) {
-            return res.render('download', { error: 'Link has been expired.' });
-        }
-
-        return res.render('download', {
-            fileName: file.filename,
-            fileSize: file.size,
-            download: `${process.env.APP_BASE_URL}/files/${file.uuid}`
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).render('download', { error: 'Something went wrong.' });
+        // Link expired
+        if(!file) {
+            return res.render('download', { error: 'Link has been expired.'});
+        } 
+        return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
+    } catch(err) {
+        return res.render('download', { error: 'Something went wrong.'});
     }
 });
 
-module.exports = router;
 
+module.exports = router;
