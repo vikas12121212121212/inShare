3ee73e4626e2af10fb4077389fc8f215d5177d42
vikas <vikasbiradar910@gmail.com>
2024-07-22 22:55:33 +0530

    y

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -123,7 +123,7 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                                                                     <tbody>
                                                                         <tr>
                                                                             <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;">
-                                                                                <a href="${downloadLink}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">Download file</a>
+                                                                                <a href="${ downloadLink }" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">Download file</a>
                                                                             </td>
                                                                         </tr>
                                                                     </tbody>
